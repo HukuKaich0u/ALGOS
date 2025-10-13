6594fa2f2309f7e2fdf6ce7080defdac56960379
HukuKaich0u <kouki0802.ao@gmail.com>
2025-10-13 11:17:37 +0900

    delete a meaningless dir

--- problems/tessoku-book/rust/target/debug/deps/rust-4878584e5d7ab25f.d
+++
@@ -1,5 +0,0 @@
-/Users/KokiAoyagi/Desktop/ALGOS/problems/tessoku-book/rust/target/debug/deps/rust-4878584e5d7ab25f.d: src/main.rs
-
-/Users/KokiAoyagi/Desktop/ALGOS/problems/tessoku-book/rust/target/debug/deps/rust-4878584e5d7ab25f: src/main.rs
-
-src/main.rs:
