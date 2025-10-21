f369b9dee51c941412103b31236d280784a41690
HukuKaich0u <kouki0802.ao@gmail.com>
2025-10-21 22:35:44 +0900

    save

--- problems/atcoder/abc/abc400/a/main.rs
+++ problems/atcoder/abc/abc400/a/main.rs
@@ -1,11 +1,20 @@
-fn main() {
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    let n: i32 = input.trim().parse().unwrap();
-    
-    if 400 % n == 0 {
-        println!("{}", 400 / n);
-    } else {
-        println!("-1");
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[i + 1..];
+        }
     }
+    &s[..]
+}
+
+fn main() {
+    let mut s = String::from("Hello world");
+
+    let word = second_word(&s);
+
+    (&mut s).clear();
+
+    println!("The second word is: {}", word)
 }
